88021cdc078292d4cf8485bd098ad70eb04a5f82
Mehdi <mehdi.behrooz@gmail.com>
2023-03-21 06:43:44 +0330

    holidays: first commit

---
+++ holidays/generate.py
@@ -0,0 +1,85 @@
+import i18n
+import argparse
+import sys
+import re
+import datetime
+import uuid
+
+CALENDAR = '''
+BEGIN:VCALENDAR
+PRODID:-//Google Inc//Google Calendar 70.9054//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:Holidays
+X-WR-TIMEZONE:Asia/Tehran{events}
+END:VCALENDAR
+'''
+
+EVENT = '''
+BEGIN:VEVENT
+DTSTART;VALUE=DATE:{start}
+DTEND;VALUE=DATE:{end}
+DTSTAMP:20171222T225127Z
+UID:{uid}
+CREATED:20171222T211914Z
+DESCRIPTION:
+LAST-MODIFIED:20171222T225127Z
+LOCATION:
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:{title}
+TRANSP:TRANSPARENT
+END:VEVENT
+'''
+
+
+def run(input_file):
+    persian_days = parse(input_file)
+    days = [(convert_persian_date(persian_date), title) for (persian_date, title) in persian_days]
+    
+    events = ""
+    for (date, title) in days:
+        start = date.strftime("%Y%m%d")
+        end = (date + datetime.timedelta(days=1)).strftime("%Y%m%d")
+        uid = "{}@google.com".format(uuid.uuid4().hex)
+        event = EVENT.format(start=start, end=end, uid=uid, title=title)
+        events += event
+
+    calendar = CALENDAR.format(events=events)
+    
+    # remove empty lines
+    calendar = "".join([s for s in calendar.splitlines(True) if s.strip("\r\n")])
+    
+    print(calendar)
+
+
+def parse(input_file):
+    try:
+        file = open(input_file, "r")
+        lines = file.read().splitlines()
+        days = []
+        for line in lines:
+            line = line.strip()
+            if not ':' in line:
+                continue
+            if line.strip() == '':
+                continue
+            date, title = re.split(': ', line)
+            days.append([date, title])
+        return days
+    except IOError:
+        return {}
+
+def convert_persian_date(persian_date):
+    year, month, day = persian_date.split('/')
+    date = i18n.to_gregorian(int(year), int(month), int(day))
+    return date
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate google calendar for holidays")
+    parser.add_argument('input')
+    args = parser.parse_args()
+    input_file = args.input
+    run(input_file)
---
+++ holidays/i18n.py
@@ -0,0 +1,35 @@
+import datetime
+
+EPOCH = 226895
+
+def to_gregorian(year, month, day):
+    """Returns a date object corresponding to a specified Persian date."""
+    k = EPOCH - 1
+    k += 365 * (year - 1)
+    k += (8 * year + 21) // 33
+    if month <= 7:
+        k += 31 * (month - 1)
+    else:
+        k += 30 * (month - 1) + 6
+    k += day
+
+    return datetime.date.fromordinal(k)
+
+
+def to_jalali(date):
+    """Returns a tuple of year, month, and day for a specified
+    datetime object."""
+    if isinstance(date, datetime.datetime):
+        date = date.date()
+    year = (33 * (date.toordinal() - EPOCH) + 3) / 12053 + 1
+    day = (date - persiandate(year, 1, 1)).days
+    if day < 216:
+        month = day // 31 + 1
+        day = day % 31 + 1
+    else:
+        month = (day-6) // 30 + 1
+        day = (day-6) % 30 + 1
+    
+    return (year, month, day)
+
+
